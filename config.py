8927d5edc55f05c24de19f8ea8920a283cfe951c
cssourav <cssourav450@gmail.com>
2026-05-12 11:39:19 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+# Bot Configuration
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '8690652847:AAGCR4XDtTNkzU1qdgOS1dXwlI0YUwLvwvg')
+OWNER_USERNAME = 'help_sourav'
+
+# Required Channels Setup
+REQUIRED_CHANNELS = [
+    {"name": "IncomeTech X", "username": "@incometechx", "url": "https://t.me/incometechx"},
+    {"name": "IncomeTech Chat", "username": "@IncomeTech_Chat", "url": "https://t.me/IncomeTech_Chat"}
+]
